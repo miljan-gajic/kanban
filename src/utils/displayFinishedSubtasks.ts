ff98b3e8cf3fa5afc48d03d0c8225db2b167e89b
Miljan Gajic <miljan.gajic@bsdex.de>
2023-07-21 13:38:53 +0200

    moved all UI to Frontend folder

--- src/utils/displayFinishedSubtasks.ts
+++
@@ -1,8 +0,0 @@
-import { Subtasks } from "@/components/BoardContent/Column/Column";
-
-export const doneSubtasks = (subtasks: Subtasks[]) => {
-  return subtasks
-    .map((sub) => sub.status === "done")
-    .flat()
-    .filter(Boolean).length;
-};
